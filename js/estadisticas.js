32f4e8be6a63d76d684cf0ad8bbbbaba3a866c54
KuromiDL <achaconr@ucenfotec.ac.cr>
2020-10-26 15:33:18 -0600

    o;ih

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -3,22 +3,28 @@
 const estadisticas_cards = document.querySelector('#estadisticas');
 
 const imprimir_estadisticas = (campeones_estadistica) => {
-    estadisticas.innnerHTML = '';
-    campeones_estadistica.forEach(obj_campeon =>{
-        let bestHp = campeones.reduce((previo, actual)=>{
-            if (actual["stats"].hp > previo["stats"].hp) {
+    estadisticas_cards.innnerHTML = '';
+    
+    campeones.forEach(obj_campeon =>{
+        let cardStats = document.createElement('div');
+        cardStats.classList.add('cardStats');
+        let bestHp = campeones.reduce(function(previo, actual) {
+            if (actual (obj_campeon)["stats"].hp > previo["stats"].hp){
                 return actual;
             }else {
                 return previo;
 
             }
+          
         })
+        let CampeonDeHP = document.createElement('div');
+        CampeonDeHP.innerText = (bestHp);
+       
+        cardStats.appendChild(CampeonDeHP);
+        estadisticas_cards.appendChild(cardStats);
     });
-    let cardStats = document.createElement('div');
-    cardStats.classList.add('cardStats');
-    let CampeonDeHP = Document.createElement('div');
-    CampeonDeHP.innerText = bestHp;
+    
+   
 
 }
-cardStats.appendChild(CampeonDeHP);
-cardStats.appendChild(estadisticas_cards);
+imprimir_estadisticas();
