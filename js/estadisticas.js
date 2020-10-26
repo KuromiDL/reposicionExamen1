f3a97f9626af64e06025eb53d0eef70469b71b64
KuromiDL <achaconr@ucenfotec.ac.cr>
2020-10-26 10:48:05 -0600

    Update estadisticas.js

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -4,6 +4,21 @@ const estadisticas_cards = document.querySelector('#estadisticas');
 
 const imprimir_estadisticas = (campeones_estadistica) => {
     estadisticas.innnerHTML = '';
-    let
+    campeones_estadistica.forEach(obj_campeon =>{
+        let bestHp = campeones.reduce((previo, actual)=>{
+            if (actual["stats"].hp > previo["stats"].hp) {
+                return actual;
+            }else {
+                return previo;
+
+            }
+        })
+    });
+    let cardStats = document.createElement('div');
+    cardStats.classList.add('cardStats');
+    let CampeonDeHP = Document.createElement('div');
+    CampeonDeHP.innerText = bestHp;
 
 }
+cardStats.appendChild(CampeonDeHP);
+cardStats.appendChild(estadisticas_cards);
