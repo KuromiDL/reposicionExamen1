008814e253e1909b42ab9190f5c648c20dedcb93
KuromiDL <achaconr@ucenfotec.ac.cr>
2020-10-26 20:32:04 -0600

    f

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -7,24 +7,28 @@ const imprimir_estadisticas = (campeones_estadistica) => {
     
     campeones.forEach(obj_campeon =>{
         let cardStats = document.createElement('div');
-        cardStats.classList.add('cardStats');
-        let bestHp = campeones.reduce(function(previo, actual) {
-            if (actual (obj_campeon)["stats"].hp > previo["stats"].hp){
-                return actual;
-            }else {
-                return previo;
+       cardStats.classList.add('cardStats');
+       let CampeonDeHP = obj_campeon.reduce((campeonAnterior, CampeonSiguiente) =>{
+       if (CampeonSiguiente["stats"].mp < campeonAnterior["stats"].mp) {
+           return CampeonSiguiente;
+       } else {
+           return campeonAnterior;
+       }
+       });
+       console.log(CampeonDeHP);
+       //if (obj_campeon["stats"].mp )
 
-            }
+        /*document.getElementsByClassName("cardStats").innnerHTML = campeones.reduce(myFunc);
+        function myFunc(bestHp, (obj_campeon)["stats"].hp); {
+            if ( bestHp < obj_campeon)["stats"].hp) 
+            return bestHp;
+        }*/
           
-        })
-        let CampeonDeHP = document.createElement('div');
+        /*let CampeonDeHP = document.createElement('div');
         CampeonDeHP.innerText = (bestHp);
        
         cardStats.appendChild(CampeonDeHP);
-        estadisticas_cards.appendChild(cardStats);
+        estadisticas_cards.appendChild(cardStats);*/
     });
-    
-   
-
 }
 imprimir_estadisticas();
